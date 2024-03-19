824800303082ba007031f744e726aba5c2120f8b
giolagioia <giovanni.lagioia@mail.polimi.it>
2024-03-19 18:29:39 +0100

    GameField class implementation

--- CodexNaturalis/src/main/java/it/polimi/ingsw/map/GameField.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/map/GameField.java
@@ -1,6 +1,15 @@
 import java.util.Map;
 import java.util.HashMap;
 public class GameField{
-    public Map<Point,Card> = new HashMap<Point,Card>;
+    private final Map<Point,Card> cards;
+    private final Map<Point,Symbol> angles;
+    private final Map<Symbol,int> symbolCounters;
+
+    public GameField(initialCard Card){
+        this.cards = new HashMap<>;
+        this.angles= new HashMap<>;
+    }
+
+
 
 }
