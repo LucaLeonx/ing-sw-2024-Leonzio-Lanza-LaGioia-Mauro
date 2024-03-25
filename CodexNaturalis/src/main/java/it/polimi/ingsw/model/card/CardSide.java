bbdb45865a5f9dbd1b252e8cb3ed1b098f1c5924
LucaLeonx <luca.leonzio@mail.polimi.it>
2024-03-25 23:01:48 +0100

    Fixes and documentatio on CardSide class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/CardSide.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/CardSide.java
@@ -5,23 +5,46 @@ import java.util.*;
 public class CardSide {
 
     private final Set<Symbol> centerSymbol;
-    private final   Map<AnglePosition,Symbol> angles;
+    private final Map<AnglePosition,Symbol> angles;
+    // Modificare metodo costruttore, le strutture dati non le deve prendere in input ma le deve creare lui!!!!
 
+    /**
+     * Class constructor
+     * @param centerSymbol - symbols displayed in the centre of the card
+     * @param angles -
+     */
     public CardSide(Set<Symbol> centerSymbol, Map<AnglePosition,Symbol> angles){
         this.centerSymbol = centerSymbol;
         this.angles = angles;
     }
 
+    /**
+     *
+     *
+     * @return A Set with the values of the center symbols
+     */
     public Set<Symbol> getCenterSymbols(){
         return centerSymbol;
     }
 
+
+    /**
+     *
+     * @param position - is one of the 4 possible angles that a cardSide could have, its type is defined by AnglePosition enum
+     * @return The symbol inside the angle given in input
+     */
     public Symbol getSymbolFromAngle(AnglePosition position){
         return angles.get(position);
     }
 
+    /**
+     *
+     * @return All the symbols displayed in the card, both the center and the angles ones,
+     * note that the returned Set is a copy of the class attribute, NOT the same, so
+     * '==' statement will return false (as opposed to equals() method)
+     */
     public Set<Symbol> getDisplayedSymbols(){
-        Set<Symbol> displayedSymbols = new HashSet<>(getDisplayedSymbols());
+        Set<Symbol> displayedSymbols = new HashSet<>(getCenterSymbols());
 
         for(AnglePosition position : AnglePosition.values()){
             displayedSymbols.add(getSymbolFromAngle(position));
