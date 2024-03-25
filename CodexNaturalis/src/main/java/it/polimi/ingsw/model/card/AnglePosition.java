05c349fda6e55f7bba371a198dbd8969e4d680fe
Stefano Lanza <steve.lanza107@tutanota.com>
2024-03-25 20:40:56 +0100

    Added AnglePosition class

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/AnglePosition.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/AnglePosition.java
@@ -1,11 +1,29 @@
 package it.polimi.ingsw.model.card;
 
+import it.polimi.ingsw.model.map.Point;
+
 public enum AnglePosition {
-    UP_LEFT(Point(-1, 1)),
-    UP_RIGHT(Point(1, 1)),
-    DOWN_LEFT(Point(-1, -1)),
-    DOWN_RIGHT(Point(1, -1));
+    /**
+     * This enum represents the possible position of the angles of a Card.
+     *
+     */
+    UP_LEFT(new Point(-1, 1)),
+    UP_RIGHT(new Point(1, 1)),
+    DOWN_LEFT(new Point(-1, -1)),
+    DOWN_RIGHT(new Point(1, -1));
 
+    private final Point relativePosition;
 
+    AnglePosition(Point relativePosition){
+        this.relativePosition = relativePosition;
+    }
+
+    /**
+     * Returns
+     * @return The position of the corrisponding angle on the GameField, relative to a card
+     */
+    public Point getRelativePosition(){
+        return relativePosition;
+    }
 
 }
