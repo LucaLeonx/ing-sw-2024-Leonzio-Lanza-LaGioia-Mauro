aac584be5f82780a9d457e92e0950225e8d5db0b
LucaLeonx <luca.leonzio@mail.polimi.it>
2024-04-13 20:30:37 +0200

    Card reorg

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/CardFactory.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/CardFactory.java
@@ -5,14 +5,17 @@ import java.util.*;
 public abstract class CardFactory {
 
 
-    private static CardSide createBackSide(Symbol centerSymbol){
-        return new CardSide(new HashSet<>().add(centerSymbol), Map.of(
+    /*
+            return new CardSide(new HashSet<>().add(centerSymbol), Map.of(
                 AnglePosition.UP_LEFT, Symbol.BLANK,
                 AnglePosition.UP_RIGHT, Symbol.BLANK,
                 AnglePosition.DOWN_LEFT, Symbol.BLANK,
                 AnglePosition.DOWN_RIGHT, Symbol.BLANK
         ));
-    }
+     */
+
+
+    private static CardSide createBackSide(Symbol centerSymbol){};
 
     private static List<Card> getCardList(){
         return List.of();
