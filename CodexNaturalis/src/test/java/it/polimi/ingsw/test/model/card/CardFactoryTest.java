7fcb9aeba6cd1396f8ef9bc58087e48cc1733d92
LucaLeonx <luca.leonzio@mail.polimi.it>
2024-04-21 19:26:13 +0200

    CardFactoryTest for Initial Card

--- CodexNaturalis/src/test/java/it/polimi/ingsw/test/model/card/CardFactoryTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/test/model/card/CardFactoryTest.java
@@ -1,14 +1,20 @@
 package it.polimi.ingsw.test.model.card;
 
-import it.polimi.ingsw.model.card.Card;
-import it.polimi.ingsw.model.card.CardFactory;
-import it.polimi.ingsw.model.card.RandomPicker;
+import it.polimi.ingsw.model.card.*;
+import junit.framework.TestCase;
 
 import java.io.FileNotFoundException;
 import java.util.Optional;
+import java.util.Set;
 
-public class CardFactoryTest {
-    public static void main(String[] args) throws FileNotFoundException {
+public class CardFactoryTest extends TestCase {
+    private Set<Integer> Ids;
+    private Set<CardColor> colors;
+
+    public void setUp(){
+
+    }
+    public void testInitialCard() throws FileNotFoundException {
 
         RandomPicker<Card> initalCardPicker = CardFactory.getInitialCards();
         Optional<Card> Optcarta;
@@ -18,8 +24,15 @@ public class CardFactoryTest {
             Optcarta = initalCardPicker.extractRandomElement();
             if(Optcarta.isPresent()) {
                 carta = Optcarta.get();
-                System.out.println("id :"+ carta.getId());
+                System.out.println("\n\nid: "+ carta.getId());
+                System.out.println("Color: "+ carta.getCardColor().toString());
+                Set<Symbol> frontCenter = carta.getSide(CardOrientation.valueOf("FRONT")).getCenterSymbols();
+                System.out.print("Front center symbols: ");
+                for(Symbol s: frontCenter){ System.out.print(" "+ s.toString());}
 
+                Set<Symbol> backDisplayedSymbols = carta.getSide(CardOrientation.valueOf("BACK")).getDisplayedSymbols();
+                System.out.print("\nAll displayed symbols in the back: ");
+                for(Symbol s: backDisplayedSymbols){ System.out.print(" "+ s.toString());}
             }
         }
 
