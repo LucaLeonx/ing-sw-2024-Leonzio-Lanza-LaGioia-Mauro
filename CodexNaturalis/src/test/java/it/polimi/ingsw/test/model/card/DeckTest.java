0823db1fb1296b506dd11c563a746b3414a42af9
Stefano Lanza <steve.lanza107@tutanota.com>
2024-04-23 12:27:10 +0200

    Created draft tests

--- CodexNaturalis/src/test/java/it/polimi/ingsw/test/model/card/DeckTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/test/model/card/DeckTest.java
@@ -1,15 +1,31 @@
 package it.polimi.ingsw.test.model.card;
 
+import it.polimi.ingsw.model.card.Card;
 import it.polimi.ingsw.model.card.Deck;
+import static it.polimi.ingsw.model.card.CardColor.*;
 import junit.framework.*;
 
 import java.util.*;
 public class DeckTest extends TestCase{
-    public void setUp(){
 
+    private Deck deck;
+    private Deck oneCardDeck;
+    private Deck emptyDeck;
+    public void setUp(){
+        deck = new Deck(List.of(
+                new Card(1, RED, null, null),
+                new Card(2, SKYBLUE, null, null),
+                new Card(3, GREEN, null, null),
+                new Card(4, GREEN, null, null)
+        ));
+
+        oneCardDeck = new Deck(List.of(new Card(5, WHITE, null, null)));
+        emptyDeck = new Deck(new HashSet<>());
     }
 
-    public void testDeckConstruction(){
-        
+    public void testIsEmpty(){
+        assertFalse(deck.isEmpty());
+        assertFalse(oneCardDeck.isEmpty());
+        assertTrue(emptyDeck.isEmpty());
     }
 }
