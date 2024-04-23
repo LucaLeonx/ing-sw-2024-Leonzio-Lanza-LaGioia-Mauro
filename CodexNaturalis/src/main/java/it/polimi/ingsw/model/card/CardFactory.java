e156fe426d54aecf4313e16e556bff397d65cec2
LucaLeonx <luca.leonzio@mail.polimi.it>
2024-04-23 16:13:05 +0200

    Method getGoldCard form cardfactory, to test

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/CardFactory.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/CardFactory.java
@@ -11,8 +11,7 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
-import static it.polimi.ingsw.model.card.GameFunctionFactory.createPointsRewardFunction;
-import static it.polimi.ingsw.model.card.GameFunctionFactory.createRequiredSymbolsFunction;
+import static it.polimi.ingsw.model.card.GameFunctionFactory.*;
 
 public abstract class CardFactory {
 
@@ -47,8 +46,19 @@ public abstract class CardFactory {
 
         Card[] card = gson.fromJson(reader,Card[].class);
         return  Arrays.asList(card);
-
     }
+
+    public static List<Card> getGoldCards() throws FileNotFoundException{
+        FileReader reader= new FileReader("src/main/java/it/polimi/ingsw/model/card/JsonFiles/Gold.json");
+        GsonBuilder builder = new GsonBuilder();
+
+        builder.registerTypeAdapter(Card[].class,new GoldCardAdapter());
+        Gson gson = builder.create();
+
+        Card[] card = gson.fromJson(reader,Card[].class);
+        return  Arrays.asList(card);
+    }
+
     private static List<Card> getCardList(){
         return List.of();
     }
@@ -227,5 +237,113 @@ class InitialCardAdapter extends TypeAdapter<Card[]>{
     }
 }
 
+class GoldCardAdapter extends TypeAdapter<Card[]>{
+    public  Card[] read (JsonReader reader) throws IOException{
+        Card[] array = new Card[6];
+        int i=0;
+        String typeName = null;
+        reader.beginArray();
 
+
+        while (reader.hasNext()) {
+            int id = 0;
+            RewardFunction reward;
+            Map<Symbol,Integer> requiredSymbols = new HashMap<Symbol,Integer>();
+            CardColor col = null;
+            CardSide front = null;
+            CardSide back = new CardSide(
+                    new HashSet<Symbol>(),
+                    Map.of(AnglePosition.UP_LEFT, Symbol.BLANK,
+                            AnglePosition.UP_RIGHT, Symbol.BLANK,
+                            AnglePosition.DOWN_LEFT, Symbol.BLANK,
+                            AnglePosition.DOWN_RIGHT, Symbol.BLANK),
+                    createRequiredSymbolsFunction(new HashMap<Symbol, Integer>()),
+                    createPointsRewardFunction(0)
+            );
+
+            reader.beginObject();
+            JsonToken token = reader.peek();
+            if(token.equals(JsonToken.NAME)){
+                typeName = reader.nextName();
+            }
+            if(typeName.equals("id")){
+                id = reader.nextInt();
+                typeName = reader.nextName();
+            }
+            if(typeName.equals("color")){
+                col = CardColor.valueOf(reader.nextString());
+                typeName = reader.nextName();
+            }
+            if(typeName.equals("front")){
+                Map<AnglePosition,Symbol> angle = new HashMap<AnglePosition,Symbol>();
+                reader.beginObject();
+
+                reader.nextName(); //RewardFunction
+                reader.beginObject();
+                reader.nextName();//Rewardtype
+                String tipoReward = reader.nextString();
+                if(tipoReward.equals("COVEREDANGLES")){
+                    reader.nextName();
+                    reader.nextInt();
+                    reward = createCoveredAnglesFunction();
+                } else if (tipoReward.equals("DEFAULT")) {
+                    reader.nextName();
+                    reward = createPointsRewardFunction(reader.nextInt());
+                } else{
+                    reward = createCountSymbolsFunction(Symbol.valueOf(tipoReward));
+                    reader.nextName();
+                    reader.nextInt();
+                }
+                reader.endObject(); // End Rewardfunction
+
+                reader.nextName(); //RequiredFunction
+
+                token = reader.peek();
+                while(!token.equals(JsonToken.END_ARRAY)){
+                    Symbol reqSymb = Symbol.valueOf(reader.nextString());
+                    if(!requiredSymbols.containsKey(reqSymb)){
+                        requiredSymbols.replace(reqSymb,requiredSymbols.get(reqSymb)+1);
+                    }
+                    else{
+                        requiredSymbols.put(reqSymb,1);
+                    }
+
+                    token = reader.peek();
+                }
+                reader.endArray();
+
+                reader.nextName(); //angle
+                reader.beginObject();
+                token = reader.peek();
+                while(!token.equals(JsonToken.END_OBJECT)){
+                    angle.put(AnglePosition.valueOf(reader.nextName()),Symbol.valueOf(reader.nextString()));
+                    token = reader.peek();
+                }
+                reader.endObject();
+
+                front = new CardSide(
+                        new HashSet<Symbol>(),
+                        angle,
+                        createRequiredSymbolsFunction(requiredSymbols),
+                        reward
+                );
+
+                reader.endObject();
+                typeName = reader.nextName();
+            }
+            array[i] = new Card(id,col,front,back);
+            i++;
+            if(token.equals(JsonToken.END_OBJECT)){
+                reader.endObject();
+            }
+
+        }
+        reader.endArray();
+        return array ;
+    }
+
+    public void write(JsonWriter write, Card[] card) throws IOException {
+
+    }
+}
 
