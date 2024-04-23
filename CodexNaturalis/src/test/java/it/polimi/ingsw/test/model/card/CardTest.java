9d51c48c2e96b89b2e059a6560f350c08005df57
Stefano Lanza <steve.lanza107@tutanota.com>
2024-04-23 14:17:55 +0200

    Tested Deck class

--- CodexNaturalis/src/test/java/it/polimi/ingsw/test/model/card/CardTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/test/model/card/CardTest.java
@@ -59,4 +59,6 @@ public class CardTest extends TestCase {
 
 
 
+
+
 }
--- CodexNaturalis/src/test/java/it/polimi/ingsw/test/model/card/DeckTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/test/model/card/DeckTest.java
@@ -6,6 +6,8 @@ import static it.polimi.ingsw.model.card.CardColor.*;
 import junit.framework.*;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
 public class DeckTest extends TestCase{
 
     private Deck deck;
@@ -29,4 +31,44 @@ public class DeckTest extends TestCase{
         assertTrue(emptyDeck.isEmpty());
     }
 
+    public void testGetTopCard(){
+
+        assertTrue(deck.getTopCard().isPresent());
+
+        assertTrue(oneCardDeck.getTopCard().isPresent());
+        assertEquals(WHITE, oneCardDeck.getTopCard().get().getCardColor());
+
+        assertTrue(emptyDeck.getTopCard().isEmpty());
+    }
+
+    public void testGetNumberOfCards(){
+        assertEquals(4, deck.getNumberOfCards());
+        assertEquals(1, oneCardDeck.getNumberOfCards());
+        assertEquals(0, emptyDeck.getNumberOfCards());
+    }
+
+    public void testDraw(){
+
+        List<Card> cardList = new ArrayList<>();
+
+        while(!deck.isEmpty()){
+            assertTrue(deck.getTopCard().isPresent());
+            cardList.add(deck.draw().get());
+        }
+
+        assertTrue(deck.isEmpty());
+        assertTrue(deck.draw().isEmpty());
+        assertEquals(0, deck.getNumberOfCards());
+        assertTrue(deck.getTopCard().isEmpty());
+
+        List<Integer> idList = cardList.stream().map(Card::getId).sorted().toList();
+        assertEquals(idList, List.of(1,2,3,4));
+
+        if(!oneCardDeck.isEmpty()) {
+            Card card = oneCardDeck.draw().get();
+            assertTrue(oneCardDeck.getTopCard().isEmpty());
+        }
+
+        assertTrue(emptyDeck.draw().isEmpty());
+    }
 }
