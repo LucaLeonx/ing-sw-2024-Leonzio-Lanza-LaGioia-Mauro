64a6172ccfe6dbf46e8bc601dca4b35500b91f09
g.lagioia <giovannilagioia03@gmail.com>
2024-04-29 12:54:26 +0200

    GameField and AngleCell documentation

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/map/AngleCell.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/map/AngleCell.java
@@ -2,6 +2,13 @@ package it.polimi.ingsw.model.map;
 
 import it.polimi.ingsw.model.card.Symbol;
 
+/**
+ * Class used by the Gamefiled to memorize the symbols in a specific point, because they can be overlapped
+ * @param topCardPosition
+ * @param bottomCardPosition
+ * @param topSymbol
+ * @param bottomSymbol
+ */
 public record AngleCell(Point topCardPosition, Point bottomCardPosition, Symbol topSymbol, Symbol bottomSymbol) {
     public AngleCell(Point topCardPosition, Symbol topSymbol){
         this(topCardPosition, topCardPosition, topSymbol, topSymbol);
