dddbe8db30d02346bc585ba1089b923e721e3bd9
Stefano Lanza <steve.lanza107@tutanota.com>
2024-05-09 16:21:25 +0200

    Created FrontierServerLayer

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/servercontroller/FrontierServerLayer.java
@@ -0,0 +1,78 @@
+package it.polimi.ingsw.controller.servercontroller;
+
+import it.polimi.ingsw.dataobject.*;
+import it.polimi.ingsw.model.DrawChoice;
+import it.polimi.ingsw.model.card.CardOrientation;
+import it.polimi.ingsw.model.card.ObjectiveCard;
+import it.polimi.ingsw.model.map.Point;
+
+import javax.print.AttributeException;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class FrontierServerLayer {
+    private final FrontierServerLayer[] nextLayers;
+
+    public FrontierServerLayer(FrontierServerLayer... nextLayers) {
+        this.nextLayers = nextLayers;
+    }
+
+    public int createLobby(User user, String lobbyName, int requiredPlayersNum){
+        return nextLayers[0].createLobby(user, lobbyName, requiredPlayersNum);
+    }
+
+    public void joinLobby(User user, int lobbyId){
+        nextLayers[0].joinLobby(user, lobbyId);
+    }
+
+    public void exitFromLobby(User user){
+        nextLayers[0].exitFromLobby(user);
+    }
+
+    public List<LobbyInfo> getLobbies(User user){
+        return nextLayers[0].getLobbies(user);
+    }
+
+    public LobbyInfo getJoinedLobbyInfo(User user){
+        return nextLayers[0].getJoinedLobbyInfo(user);
+    }
+
+    public String getCurrentPlayer(User user){
+        return nextLayers[0].getCurrentPlayer(user);
+    }
+
+    public ControlledPlayerInfo getControlledPlayerInfo(User user){
+        return nextLayers[0].getControlledPlayerInfo(user);
+    }
+
+    public OpponentInfo getOpponentInfo(User user, String opponentName){
+        return nextLayers[0].getOpponentInfo(user, opponentName);
+    }
+
+    public DrawableCardsInfo getDrawableCardsInfo(User user){
+        return nextLayers[0].getDrawableCardsInfo(user);
+    }
+
+    public PlayerSetupInfo getPlayerSetupInfo(User user){
+        return nextLayers[0].getPlayerSetupInfo(user);
+    }
+
+    public boolean isLastTurn(User user){
+        return nextLayers[0].isLastTurn(user);
+    }
+
+    public boolean hasGameEnded(User user){
+        return nextLayers[0].hasGameEnded(user);
+    }
+
+    public void registerPlayerSetup(User user, int objectiveCardId, CardOrientation initialCardSide){
+        nextLayers[0].registerPlayerSetup(user, objectiveCardId, initialCardSide);
+    }
+
+    public void registerPlayerMove(User user, int placedCardId, Point placementPoint, CardOrientation chosenSide, DrawChoice drawChoice){
+        nextLayers[0].registerPlayerMove(user, placedCardId, placementPoint, chosenSide, drawChoice);
+    }
+    public String getWinnerName(User user){
+        return nextLayers[0].getWinnerName(user);
+    }
+}
