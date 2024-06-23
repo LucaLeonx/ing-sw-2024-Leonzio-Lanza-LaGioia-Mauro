3c96198dfe668a1065d381b44daab1bae45afec9
smaur <Simone.Mauro@mail.polimi.it>
2024-06-23 11:41:46 +0200

    OtherMapsPanel frame: removed useless cardPanel

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/gui/OtherMapsFrame.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/gui/OtherMapsFrame.java
@@ -6,32 +6,14 @@ import java.rmi.RemoteException;
 
 public class OtherMapsFrame extends JFrame {
     private static OtherMapsPanel otherMapsPanel;
-    private static CardLayout cardLayout;
-    private static JPanel cardPanel;
 
 
     public OtherMapsFrame(String oppName) throws RemoteException {
         otherMapsPanel = new OtherMapsPanel();
         otherMapsPanel.buildPanel(oppName);
-        cardLayout = new CardLayout();
-        cardPanel = new JPanel(cardLayout);
-        cardPanel.add("otherMapsPanel", otherMapsPanel);
-        add(cardPanel);
+        this.add(otherMapsPanel);
         setVisible(true);
-
         this.setSize(1500, 800);
-    }
-
-    public static CardLayout getCardLayout(){
-        return cardLayout;
-    }
-
-    public static JPanel getJPanel(){
-        return cardPanel;
-    }
-
-    public static void goToWindow(String name) {
-        cardLayout.show(cardPanel, name);
     }
 
 
